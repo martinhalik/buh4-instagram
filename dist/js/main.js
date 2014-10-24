90475f87ac055255e70210504f6608cd036f1342
martinhalik <martin@mh1.cz>
2014-10-24 11:28:57 +0200

    instagram tag running

--- dist/js/main.js
+++ dist/js/main.js
@@ -19,10 +19,10 @@ var clientId = 'fbd20b88d2ab49b7acc2f9732a03e525';
 
 console.log('56');
 
-var getPhotos = function (callback) {
+/*var getPhotos = function (callback) {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', api + 'tags/brno/media/recent?client_id=' + clientId);
-  xhr.setRequestHeader('X-Auth-Token', token);
+  xhr.setRequestHeader('X-Insta-Forwarded-For', );
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
       var json = JSON.parse(xhr.responseText),
@@ -39,3 +39,19 @@ getPhotos( function(photos) {
   console.log(photos);
 
   });
+*/
+
+
+// Get an Instagram API client.
+var instagram = new Instagram(clientId);
+
+// Get tagged media
+instagram.getTaggedImages( function(images) {
+
+  for (var i = 0, len = images.length; i < len; i++) {
+    var img = document.createElement('img');
+    img.src = images[i].imageUrl;
+    document.body.appendChild(img);
+  }
+
+}, true);
